0bdd3621d990177e3488a569e94985df9636beb8
baumiger <167179467+baumiger@users.noreply.github.com>
2024-04-16 10:09:10 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    let sprite: game.LedSprite = null
+    sprite.move(1)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
+let o1 = game.createSprite(0, 0)
+basic.forever(function () {
+	
+})
